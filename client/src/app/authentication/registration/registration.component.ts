e01e2c2796374345b9be96402cb6f4b940686fd0
Sumedhabelur <sumedhabelur@gmail.com>
2020-01-06 21:17:49 +0530

    Added buttons in Dashboard Component

--- client/src/app/authentication/registration/registration.component.ts
+++ client/src/app/authentication/registration/registration.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { HttpService } from "src/app/shared/http.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  selector: "app-registration",
+  templateUrl: "./registration.component.html",
+  styleUrls: ["./registration.component.css"]
 })
 export class RegistrationComponent implements OnInit {
+  registrationForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private httpService: HttpService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.registrationForm = this.fb.group({
+      userName: ["", [Validators.required]],
+      pass: ["", [Validators.required]],
+      firstName: ["", [Validators.required]],
+      lastName: ["", [Validators.required]],
+      dob: ["", [Validators.required]],
+      email: ["", [Validators.required]],
+      typeOfAdmission: ["", [Validators.required]],
+      class: ["", [Validators.required]],
+      dept: ["", [Validators.required]]
+    });
   }
 
+  registerUser() {}
 }
--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
+  selectedType = 'admin';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  selectAuthType(type){
+    console.log('jhhhgjhgg')
+    this.selectedType = type;
   }
-
 }
